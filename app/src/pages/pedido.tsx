803eff5c5146e44f3289f0d7ba3d9f164938fd6c
Karim-Neme <101999108+Karim-Neme@users.noreply.github.com>
2023-11-04 17:32:13 -0300

    hice un intento de inicio del front end del chek out en pedido y habia hecho uno para la lista de pedidos pero lo voy a cambiar asique la pagina que dice historial esta practicamente vacia

---
+++ app/src/pages/pedido.tsx
@@ -0,0 +1,119 @@
+
+
+export default function Pedido() {
+  return (
+     <>
+     
+     
+
+     
+
+
+     <div className="relative mx-auto w-full ">
+  <div className="grid min-h-screen grid-cols-10">
+    <div className="col-span-full py-6 px-4 sm:py-12 lg:col-span-6 lg:py-24">
+      <div className="mx-auto w-full max-w-lg">
+        <h1 className="relative text-2xl font-medium text-white sm:text-3xl">Checkout<span className="mt-2 block h-1 w-10 bg-teal-600 sm:w-20"></span></h1>
+        <form action="" className="mt-10 flex flex-col space-y-4">
+        <div><label htmlFor="direccion" className="text-xs font-semibold text-gray-500">Direccion de envio</label><input type="direccion" id="direccion" name="direccion" placeholder="Calle falsa 123" className="mt-1 block w-full rounded border-gray-300 bg-black py-3 px-4 text-sm placeholder-gray-300 shadow-sm outline-none transition focus:ring-2 focus:ring-teal-500" /></div>
+          <div className="relative"><label htmlFor="card-number" className="text-xs font-semibold text-gray-500">Numero de tarjeta</label><input type="text" id="card-number" name="card-number" placeholder="1234-5678-XXXX-XXXX" className="block w-full rounded border-gray-300 bg-black py-3 px-4 pr-10 text-sm placeholder-gray-300 shadow-sm outline-none transition focus:ring-2 focus:ring-teal-500" /><img src="/images/uQUFIfCYVYcLK0qVJF5Yw.png" alt="" className="absolute bottom-3 right-3 max-h-4" /></div>
+          <div>
+            <p className="text-xs font-semibold text-gray-500">Fecha de vencimiento</p>
+            <div className="mr-6 flex flex-wrap">
+              <div className="my-1">
+                <label htmlFor="month" className="sr-only">Select expiration month</label
+                ><select name="month" id="month" className="cursor-pointer rounded border-gray-300 bg-black py-3 px-2 text-sm shadow-sm outline-none transition focus:ring-2 focus:ring-teal-500">
+                  <option value="">01</option>
+                  <option value="">02</option>
+                  <option value="">03</option>
+                  <option value="">04</option>
+                  <option value="">05</option>
+                  <option value="">06</option>
+                  <option value="">07</option>
+                  <option value="">08</option>
+                  <option value="">09</option>s
+                  <option value="">10</option>
+                  <option value="">11</option>
+                  <option value="">12</option>
+                </select>
+              </div>
+              <div className="my-1 ml-3 mr-6">
+                <label htmlFor="year" className="sr-only">Select expiration year</label
+                ><select name="year" id="year" className="cursor-pointer rounded border-gray-300 bg-black py-3 px-2 text-sm shadow-sm outline-none transition focus:ring-2 focus:ring-teal-500">
+                  <option value="">23</option>
+                  <option value="">24</option>
+                  <option value="">25</option>
+                  <option value="">26</option>
+                  <option value="">27</option>
+                  <option value="">28</option>
+                  <option value="">29</option>
+                  <option value="">30</option>
+                </select>
+              </div>
+              <div className="relative my-1"><label htmlFor="security-code" className="sr-only">Security code</label><input type="text" id="security-code" name="security-code" placeholder="Codigo de seguridad" className="block w-360 rounded bg-black border-gray-300 bg-gray-50 py-3 px-4 text-sm placeholder-gray-300 shadow-sm outline-none transition focus:ring-2 focus:ring-teal-500" /></div>
+            </div>
+          </div>
+          <div><label htmlFor="card-name" className="text-xs font-semibold text-gray-500">Nombre en la tarjeta</label><input type="text" id="card-name" name="card-name" placeholder="Juan Perez" className="mt-1 block w-full rounded border-gray-300 bg-black py-3 px-4 text-sm placeholder-gray-300 shadow-sm outline-none transition focus:ring-2 focus:ring-teal-500" /></div>
+        </form>
+        
+        <button type="submit" className="mt-4 inline-flex w-full items-center justify-center rounded bg-teal-600 py-2.5 px-4 text-base font-semibold tracking-wide text-white text-opacity-80 outline-none ring-offset-2 transition hover:text-opacity-100 focus:ring-2 focus:ring-teal-500 sm:text-lg">Confirmar pedido</button>
+      </div>
+    </div>
+    <div className="relative col-span-full flex flex-col py-6 pl-8 pr-4 sm:py-12 lg:col-span-4 lg:py-24">
+      <h2 className="sr-only">Order summary</h2>
+      <div>
+        
+        <div className="absolute inset-0 h-full w-full z-[-1] bg-gradient-to-t from-teal-800 to-teal-400 opacity-95"></div>
+      </div>
+      <div className="relative">
+        <ul className="space-y-5">
+          <li className="flex justify-between">
+            <div className="inline-flex">
+              <img src="https://images.unsplash.com/photo-1620331311520-246422fd82f9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGhhaXIlMjBkcnllcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" className="max-h-16" />
+              <div className="ml-3">
+                <p className="text-base font-semibold text-white">Nano Titanium Hair Dryer</p>
+                <p className="text-sm font-medium text-white text-opacity-80">Pdf, doc Kindle</p>
+              </div>
+            </div>
+            <p className="text-sm font-semibold text-white">$260.00</p>
+          </li>
+          <li className="flex justify-between">
+            <div className="inline-flex">
+              <img src="https://images.unsplash.com/photo-1621607512214-68297480165e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fGhhaXIlMjBkcnllcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" className="max-h-16" />
+              <div className="ml-3">
+                <p className="text-base font-semibold text-white">Luisia H35</p>
+                <p className="text-sm font-medium text-white text-opacity-80">Hair Dryer</p>
+              </div>
+            </div>
+            <p className="text-sm font-semibold text-white">$350.00</p>
+          </li>
+        </ul>
+        <div className="my-5 h-0.5 w-full bg-white bg-opacity-30"></div>
+        <div className="space-y-2">
+          <p className="flex justify-between text-lg font-bold text-white"><span>Total price:</span><span>$510.00</span></p>
+          <p className="flex justify-between text-sm font-medium text-white"><span>Vat: 10%</span><span>$55.00</span></p>
+        </div>
+      </div>
+      <div className="relative mt-10 text-white">
+        <h3 className="mb-5 text-lg font-bold">Support</h3>
+        <p className="text-sm font-semibold">+01 653 235 211 <span className="font-light">(International)</span></p>
+        <p className="mt-1 text-sm font-semibold">support@nanohair.com <span className="font-light">(Email)</span></p>
+        <p className="mt-2 text-xs font-medium">Call us now for payment related issues</p>
+      </div>
+      <div className="relative mt-10 flex">
+        <p className="flex flex-col"><span className="text-sm font-bold text-white">Money Back Guarantee</span><span className="text-xs font-medium text-white">within 30 days of purchase</span></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+     
+
+
+
+
+
+     </>
+     
+  )
+}
