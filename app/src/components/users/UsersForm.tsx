d8bfe1c4225864423448ea8aac5ba38b383e5e32
claragpanella <claragpanella@gmail.com>
2023-11-06 05:32:34 -0300

    Agregado de estilos

--- app/src/components/users/UsersForm.tsx
+++ app/src/components/users/UsersForm.tsx
@@ -1,75 +1,156 @@
 import { useState, useEffect } from 'react';
 import { addUser, getUsers } from '../../services/user'; // Importa las funciones necesarias
 import UsersList from './UsersList';
+import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
 
 export default function UsersForm() {
- const [username, setUsername] = useState('');
- const [email, setEmail] = useState('');
- const [displayName, setDisplayName] = useState('');
- const [emailVerified, setEmailVerified] = useState('');
- const [phoneNumber, setPhoneNumber] = useState('');
- const [photoURL, setPhotoURL] = useState('');
- const [role, setRole] = useState(''); 
- const [users, setUsers] = useState<any>({})
- const [loading, setLoading] = useState(true);
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [displayName, setDisplayName] = useState('');
+  const [emailVerified, setEmailVerified] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [photoURL, setPhotoURL] = useState('');
+  const [role, setRole] = useState('');
+  const [users, setUsers] = useState<any>({})
+  const [loading, setLoading] = useState(true);
 
- useEffect(() => {
-  const fetchUsers = async () => {
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const users = await getUsers();
+        console.log(users);
+        setUsers(users);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+        setLoading(false);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
     try {
-      const users = await getUsers();
-      console.log(users);
-      setUsers(users);      
-      setLoading(false);
+      const userData = { username, email, displayName, emailVerified, phoneNumber, photoURL, role };
+      const response = await addUser(userData);
+      setUsers([...users, response]);
     } catch (error) {
       console.error(error);
-      setLoading(false);
     }
   };
 
-  fetchUsers();
- }, []);
-
- const handleSubmit = async (event:any) => {
-  event.preventDefault();
-  try {
-    const userData = { username, email, displayName, emailVerified, phoneNumber, photoURL, role };
-    const response = await addUser(userData); 
-    setUsers([...users, response]);
-  } catch (error) {
-    console.error(error);
+  if (loading) {
+    return <>loading...</>
   }
- };
 
- if (loading) {
-  return <>loading...</>
- }
+  return (
+    <>
+      <hr />
+      <br />
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8 ">
+        <form onSubmit={handleSubmit}>
+          <div className="space-y-12">
+            <div className="border-b border-gray-900/10 pb-12">
+              <h1 className="text-base font-semibold leading-7 text-gray-900">Registro Usuario</h1>
+              <p className="mt-1 text-sm leading-6 text-gray-600">Esta información se mostrará públicamente, así que tenga cuidado con lo que comparte.</p>
 
- return (
-  <>
-    <hr />
-    <br />
-    Formulario ingreso de usuario
-    <div className="container">
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} placeholder="Username" />
-        <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" />
-        <input type="text" value={displayName} onChange={e => setDisplayName(e.target.value)} placeholder="Display Name" />
-        <input type="text" value={emailVerified} onChange={e => setEmailVerified(e.target.value)} placeholder="Email Verified" />
-        <input type="text" value={phoneNumber} onChange={e => setPhoneNumber(e.target.value)} placeholder="Phone Number" />
-        <input type="text" value={photoURL} onChange={e => setPhotoURL(e.target.value)} placeholder="Photo URL" />
-        <select value={role} onChange={e => setRole(e.target.value)}>
-         <option value="">Selecciona un rol</option>
-         <option value="admin">Administrador</option>
-         <option value="client">Cliente</option>
-       </select>
+              <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <div className="sm:col-span-3">
+                  <label className="block text-sm font-medium leading-6 text-gray-900">First name</label>
+                  <div className="mt-2">
+                    <input type="text" value={username} onChange={e => setUsername(e.target.value)} className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Username" />
+                  </div>
+                </div>
 
-        <button type="submit">Add User</button>
-      </form>
-    </div>
-    <h1>Users List:</h1>
-      <div>
-        <UsersList/>
+                <div className="sm:col-span-3">
+                  <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
+                    Display Name
+                  </label>
+                  <div className="mt-2">
+                    <input type="text" value={displayName} onChange={e => setDisplayName(e.target.value)} className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Display Name" />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-4">
+                  <label className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                  <div className="mt-2">
+                    <input type="email" value={email} onChange={e => setEmail(e.target.value)} className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Email" />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-4">
+                  <label className="block text-sm font-medium leading-6 text-gray-900">Email Verified</label>
+                  <div className="mt-2">
+                    <input type="text" value={emailVerified} onChange={e => setEmailVerified(e.target.value)} className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Email Verified" />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-4">
+                  <label className="block text-sm font-medium leading-6 text-gray-900">Phone Number</label>
+                  <div className="mt-2">
+                    <input type="text" value={phoneNumber} onChange={e => setPhoneNumber(e.target.value)} className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Phone Number" />
+                  </div>
+                </div>
+
+                <div className="col-span-full">
+                  <label htmlFor="photo" className="block text-sm font-medium leading-6 text-gray-900">
+                    Photo
+                  </label>
+                  <div className="mt-2 flex items-center gap-x-3">
+                    <UserCircleIcon className="h-12 w-12 text-gray-300" aria-hidden="true" />
+                    <div className="mt-2">
+                      <input
+                        type="text"
+                        value={photoURL}
+                        onChange={e => setPhotoURL(e.target.value)}
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        placeholder="URL de la Foto"
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="sm:col-span-3">
+                  <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
+                    Rol
+                  </label>
+                  <div className="mt-2">
+                    <select value={role} onChange={e => setRole(e.target.value)}
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                    >
+                      <option value="">Selecciona un rol</option>
+                      <option value="admin">Administrador</option>
+                      <option value="client">Cliente</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-6 flex items-center justify-end gap-x-6">
+            <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Add User
+            </button>
+          </div>
+        </form>
       </div>
-  </>
- );
+
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8 ">
+      <div className="border-b border-gray-900/10 pb-12">
+        <div>
+          <UsersList />
+        </div>
+      </div>
+      </div>
+    </>
+  );
 }
